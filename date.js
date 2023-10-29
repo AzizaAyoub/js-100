c9b2dd47f8718c4eb0248a198b5ae15b2cc614d0
Aziza Ayoub <meaziza0@gmail.com>
2023-10-29 18:27:58 +0430

    using the Date method

---
+++ date.js
@@ -0,0 +1,9 @@
+### Solution 
+```JavaScript
+console.log(Date.now()); // return 1698587719287
+```
+
+### Discussion
+The Date.now() static method returns the number of milliseconds elapsed since the epoch, which is defined as the midnight at the beginning of January 1, 1970, UTC.
+
+
